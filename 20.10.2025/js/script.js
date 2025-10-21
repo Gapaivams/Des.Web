87625625f1298eb4a035552ec05b41b2dff94a93
GABRIEL DE PAIVA MESSIAS SILVA <gabrielsilva007@cs.up.edu.br>
2025-10-20 21:41:51 -0300

    Added: Aula 20/10/2025 - JavaScript

---
+++ 20.10.2025/js/script.js
@@ -0,0 +1,15 @@
+function exibir(){
+    document.getElementById("deslize").innerHTML = "Seu mouse esta sobre o texto"
+}
+
+function retirar(){
+    document.getElementById("deslize").innerHTML = ""
+} 
+
+function trocarCor(){
+    document.getElementById("colunaCor").style.color='red'
+}
+
+function tirarCor(){
+    document.getElementById("colunaCor").style.color='black'
+}
