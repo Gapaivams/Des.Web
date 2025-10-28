c797c82dc0498d132d61641665d56d5e7c259ad9
GABRIEL DE PAIVA MESSIAS SILVA <gabrielsilva007@cs.up.edu.br>
2025-10-27 21:32:03 -0300

    aula 27/10/2025

--- 20.10.2025/js/script.js
+++ 20.10.2025/js/script.js
@@ -1,15 +1,37 @@
 function exibir(){
-    document.getElementById("deslize").innerHTML = "Seu mouse esta sobre o texto"
+    document.getElementById("deslize").innerHTML = "Seu mouse esta sobre o texto";
 }
 
 function retirar(){
-    document.getElementById("deslize").innerHTML = ""
+    document.getElementById("deslize").innerHTML = "";
 } 
 
 function trocarCor(){
-    document.getElementById("colunaCor").style.color='red'
+    document.getElementById("colunaCor").style.color='red';
 }
 
 function tirarCor(){
-    document.getElementById("colunaCor").style.color='black'
+    document.getElementById("colunaCor").style.color='black';
+}
+
+function trocarTam(){
+
+    if(document.getElementById("colunaTam").style.fontSize == "2rem"){
+        document.getElementById("colunaTam").style.fontSize = "";
+        document.getElementById("colunaTam").querySelector("h3").style.fontSize = "";
+    }else{
+        document.getElementById("colunaTam").style.fontSize = "2rem";
+        document.getElementById("colunaTam").querySelector("h3").style.fontSize = "2rem";
+    }
+}
+
+
+function alterarLamp(){
+    let lamp = document.getElementById("lamp")
+    if(lamp.src.endsWith("img/pic_bulboff.gif")){
+        lamp.src = "img/pic_bulbon.gif"
+    } else{
+        lamp.src = "img/pic_bulboff.gif"
+    }
+
 }
